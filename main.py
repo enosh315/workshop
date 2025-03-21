a4ac45eb84ac8ee7962e604f03835eb8ae7bd611
enosh315 <varmaraju181373@gmail.com>
2025-03-21 12:53:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
 
-name = st.text_input("Enter your name", "John Doe")
+name = st.text_input("Enter your name", "Enosh")
 password = st.text_input("Enter your password", type="password")
 
 
 if st.button("Submit"):
-   if len(password) < 8:
+   if len(password) < 10:
        st.warning("Password must be at least 8 characters long")
 
 
